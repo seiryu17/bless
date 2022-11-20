76679e0cdc5665125c0ed923950d81f998497db3
Rio <rio.lim@creatory.id>
2022-11-20 16:05:07 +0700

    teststs

--- pages/pembelian/index.tsx
+++ pages/pembelian/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import LayoutComponent from "../../src/component/layout";
 
 const Pembelian = () => {
-  return <LayoutComponent activeMenuKey="5">Pembelian</LayoutComponent>;
+  return <LayoutComponent activeMenuKey="5">Pembeliann</LayoutComponent>;
 };
 
 export default Pembelian;
