3ae8f261e528af8853fb4ecc4224158d3ceed592
Rio <rio.lim@creatory.id>
2022-11-20 16:59:00 +0700

    Keep calm and commit

--- pages/pembelian/index.tsx
+++ pages/pembelian/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LayoutComponent from '../../src/component/layout'
 
 const Pembelian = () => {
-  return <LayoutComponent activeMenuKey='5'>Pembeliann</LayoutComponent>
+  return <LayoutComponent activeMenuKey='5'>Pembelian</LayoutComponent>
 }
 
 export default Pembelian
